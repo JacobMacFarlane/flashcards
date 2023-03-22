a88d1d271a9784aba0bd0a8c3a6bb2ebb4aced9a
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-03-21 18:58:35 -0700

    working with the game class

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round')
+const Turns = require('../src/Turns')
+const Deck = require('../src/Deck')
+const Card = require('../src/Card');
 
 class Game {
-  constructor() {}
+  constructor(round) {
+    this.deck = new Deck(this.createCards)
+    this.currentRound = new Round(this.deck)
+  }
+
+  start() {
+
+    const cards = prototypeQuestions.map(question => new Card(question.id,question.question, question.answers, question.correctAnswer))
+    this.deck = new Deck(cards)
+    this.currentRound = new Round(this.deck)
+  }
+  createCards() {
+    return prototypeQuestions.map(question => new Card(question.id,question.question, question.answers, question.correctAnswer))
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
